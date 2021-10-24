609396bcf3219073ca01cc9e796195adf6d3da6c
rismasri22 <rismasrinh@student.telkomuniversity.ac.id>
2021-10-24 22:47:08 +0700

    Create Home.php

---
+++ MODUL2 RISMA/Home.php
@@ -0,0 +1,118 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <title>Home</title>
+  </head>
+  <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="container d-flex justify-content-center align-item-center">
+            <div class="navbar-nav">
+                <a class="nav-link active" href="Home.php">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="Booking.php">Booking</a>
+            </div>
+            </div>
+        </div>
+        </nav>
+
+        <br>
+            <h4 style="text-align: center; color: black; font-family: Times">WELCOME TO ESD VENUE RESERVATION</h3>
+            <div class="container">
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                 <div class="container d-flex justify-content-center align-item-center">
+                   <div class="navbar-nav">
+                     <h6 style="text-align: center; color: white">Find your best deal for your event, here!</h5> 
+                     </div>
+            </div>
+        </div>
+        </nav>
+
+        <br>
+        <div class="card-deck d-flex justify-content-center align-item-center" action="booking.php" method= "get">
+        <div class="card" style="max-width: 17rem; max-height: 43rem">
+        <img src="https://drive.google.com/uc?export=view&id=1BA-PvhT1sZDrLOwUqYQWl0Yj3TTTNWRj" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title text-left">Nusantara Hall</h5>
+            <p class="card-text text-left text-info">$ 2000/Hour</p>
+            <p class="card-text text-left text-info">5000 Capacity</p> 
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item text-center">
+                <h6 style="text-align: center; color: Green; ">Free Parking</h6>
+            <li class="list-group-item text-center">
+                <h6 style="text-align: center; color: Green; ">Full AC</h6>
+            <li class="list-group-item text-center">
+                <h6 style="text-align: center; color: Green; ">Cleaning Service</h6>
+            <li class="list-group-item text-center">
+                <h6 style="text-align: center; color: Green; ">Covid-19 Health Protocol</h6>
+        </ul>
+        <div class="card-footer">
+            <a action="Booking.php" class="btn btn-primary d-flex justify-content-center align-item-center" href="Booking.php?type=Nusantara Hall" role="button">Book Now</a>
+        </div>
+        </div>
+
+        <div class="card" style="max-width: 17rem; max-height: 43rem">
+        <img src="https://drive.google.com/uc?export=view&id=1R9wNDTzcUhomEF5VK0RF2yFV1Lpk7gd4" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title text-left">Garuda Hall</h5>
+            <p class="card-text text-info text-left">$ 1000/Hour</p>
+            <p class="card-text text-info text-left">2000 Capacity</p>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item text-center">
+                <h6 style="text-align: center; color: Green; ">Free Parking</h6>
+            <li class="list-group-item text-center">
+                <h6 style="text-align: center; color: Green; ">Full AC</h6>
+            <li class="list-group-item text-center">
+                <h6 style="text-align: center; color: Red; ">No Cleaning Service</h6>
+            <li class="list-group-item text-center">
+                <h6 style="text-align: center; color: Green; ">Covid-19 Health Protocol</h6>
+        </ul>
+        <div class="card-footer">
+            <a action="Booking.php" class="btn btn-primary d-flex justify-content-center align-item-center" href="Booking.php?type=Garuda Hall" role="button">Book Now</a>
+        </div>
+        </div>
+
+        <div class="card" style="max-width: 17rem; max-height: 43rem">
+        <img src="https://drive.google.com/uc?export=view&id=1XVLxfroLQDIyOgjVHTzR5TmVge_eP5_L" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title text-left">Gedung Serba Guna</h5>
+            <p class="card-text text-left text-info">$ 500/Hour</p>
+            <p class="card-text text-left text-info">$ 500 Capacity</p>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item text-center">
+                <h6 style="text-align: center; color: Red; ">No Free Parking</h6>
+            <li class="list-group-item text-center">
+                <h6 style="text-align: center; color: Red; ">No Full AC</h6>
+            <li class="list-group-item text-center">
+                <h6 style="text-align: center; color: Red; ">No Cleaning Service</h6>
+            <li class="list-group-item text-center">
+                <h6 style="text-align: center; color: Green; ">Covid-19 Health Protocol</h6>
+        </ul>
+        <div class="card-footer">
+            <a class="btn btn-primary d-flex justify-content-center" href="Booking.php?type=Gedung Serba Guna" role="Button">Book Now</a>
+        </div>
+        </div>
+        <div class="container">
+            <div class="row pt-3">
+                <div class="col text-center">
+                    <p>Created by : risma_1202194153 </p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    </body>
+</html>
+
+        
+        
