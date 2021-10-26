c91647dbfb327f02af5fc25bc717d34e6457ad64
jackzay <59630877+jackzay@users.noreply.github.com>
2021-10-26 14:34:36 +0700

    add bugs m2

--- MODUL2 RISMA/Booking.php
+++ MODUL2 RISMA/Booking.php
@@ -1,133 +1,134 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
-    <title>Booking</title>
-  </head>
-  <body>
+        <title>Booking</title>
+    </head>
+    <body>
 
-    
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="container d-flex justify-content-center align-item-center">
-            <div class="navbar-nav">
-                <a class="nav-link" href="Home.php">Home <span class="sr-only">(current)</span></a>
-                <a class="nav-link active" href="Booking.php">Booking</a>
+        
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="container d-flex justify-content-center align-item-center">
+                <div class="navbar-nav">
+                    <a class="nav-link" href="Home.php">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link active" href="Booking.php">Booking</a>
+                </div>
+                </div>
             </div>
-            </div>
-        </div>
-        </nav>
-    
-        <section>
-        <?php
-            $username = "Risma_1202194153";
-            $photo = "https://drive.google.com/uc?export=view&id=1XVLxfroLQDIyOgjVHTzR5TmVge_eP5_L";
-        ?>
-        </section>
-        <br>
-        <section id="hotel">
-                <div class="container">
-                    <h5 class="text-center text-light  bg-dark" style="padding: 4px;">Reserve your venue now!</h5>
-                    <div class="row">
-                        <div class="col-lg-5 d-flex center">
-                            <?php if (isset($_POST['image'])) : ?>
-                                <div class = "justify-content-center">
-                                <img class="m-auto p-3"src="<?= $_POST['image'] ?>" alt="pic" height="250px" width="600px">
-                            <?php else : ?>
-                                <img class="m-auto p-3" src="<?= $photo ?>"alt="pic" height="250px" width="600px">
-                            <?php endif; ?>
+            </nav>
+        
+            <section>
+            <?php
+                $username = "Risma_1202194153";
+                $photo = "https://drive.google.com/uc?export=view&id=1XVLxfroLQDIyOgjVHTzR5TmVge_eP5_L";
+            ?>
+            </section>
+            <br>
+            <section id="hotel">
+                    <div class="container">
+                        <h5 class="text-center text-light  bg-dark" style="padding: 4px;">Reserve your venue now!</h5>
+                        <div class="row">
+                            <div class="col-lg-5 d-flex center">
+                            
+                                <?php if (iset($_POST['image'])) : ?>
+                                    <div class = "justify-content-center">
+                                    <img class="m-auto p-3"src="<?= $_POST['image'] ?>" alt="pic" height="250px" width="600px">
+                                <?php else : ?>
+                                    <img class="m-auto p-3" src="<?= $photo ?>"alt="pic" height="250px" width="600px">
+                                <?php endif; ?>
+                            </div>
                         </div>
-                    </div>
-                </div>     
-        </section>
-                        
-       </br>
+                    </div>     
+            </section>
+                            
+        </br>
 
-        <div class="container mt-3">
-        <div class="card mb-3" style="max-width: 100rem; max-height: 150rem">
-            <div class="row no-gutters">
-                <div class="col-md-7">
-                    <form action="MyBooking.php" method = "post" class ="ms-5">
-                        <div class="form-group col-md-12 mt-3">
-                            <label for="nama">Name</label>
-                            <input type="text" class="form-control" id="nama1" aria-describedby="" name = "nama" placeholder="risma_1202194xxx">
-                        </div>
-                        <div class="form-group col-md-12">
-                            <label for="eventdate">Event Date</label>
-                            <input type="date" class="form-control" id="eventdate1" name = "eventdate">
-                        </div>
-                        <div class="form-group col-md-12">
-                            <label for="starttime">Start Time</label>
-                            <input type="time" class="form-control" id="starttime1" name = "starttime">
-                        </div>
-                        <div class="form-group col-md-12">
-                            <label for="duration">Duration(Hours)</label>
-                            <input type="text" class="form-control" id="duration1" name = "duration" placeholder="1">
-                        </div>
-                        <div class="form-group col-md-12">
-                            <label for="tipe">Building Type</label>
-                            <?php
-                                if(empty($_GET['type'])){
-                            ?>
-                                <div class="form-group col-md-13">
-                                <select class="custom-select mr-sm-2" name="tipegedung" value="tipegedung" id="tipegedung1">
-                                    <option selected>Choose...</option>
-                                    <option value="Nusantara Hall">Nusantara Hall</option>
-                                    <option value="Garuda Hall">Garuda Hall</option>
-                                    <option value="Gedung Serba Guna">Gedung Serba guna</option>
-                            </select>
+            <div class="container mt-3">
+            <div class="card mb-3" style="max-width: 100rem; max-height: 150rem">
+                <div class="row no-gutters">
+                    <div class="col-md-7">
+                        <form action="MyBooking.php" method = "post" class ="ms-5">
+                            <div class="form-group col-md-12 mt-3">
+                                <label for="nama">Name</label>
+                                <input type="text" class="form-control" id="nama1" aria-describedby="" name = "nama" placeholder="risma_1202194xxx">
+                            </div>
+                            <div class="form-group col-md-12">
+                                <label for="eventdate">Event Date</label>
+                                <input type="date" class="form-control" id="eventdate1" name = "eventdate">
+                            </div>
+                            <div class="form-group col-md-12">
+                                <label for="starttime">Start Time</label>
+                                <input type="time" class="form-control" id="starttime1" name = "starttime">
+                            </div>
+                            <div class="form-group col-md-12">
+                                <label for="duration">Duration(Hours)</label>
+                                <input type="text" class="form-control" id="duration1" name = "duration" placeholder="1">
+                            </div>
+                            <div class="form-group col-md-12">
+                                <label for="tipe">Building Type</label>
+                                <?php
+                                    if(empty($_GET['type'])){
+                                ?>
+                                    <div class="form-group col-md-13">
+                                    <select class="custom-select mr-sm-2" name="tipegedung" value="tipegedung" id="tipegedung1">
+                                        <option selected>Choose...</option>
+                                        <option value="Nusantara Hall">Nusantara Hall</option>
+                                        <option value="Garuda Hall">Garuda Hall</option>
+                                        <option value="Gedung Serba Guna">Gedung Serba guna</option>
+                                </select>
+                                    </div>
+                                <?php } else { ?>
+                                    <div>
+                                        <input type="text" value="<?php echo $_GET['type'] ?>" readonly name="tipegedung" value="tipegedung">
+                                    </div> 
+                                <?php } ?>  
+                                
+                                
+                            </div>
+                            <div class="form-group col-md-12">
+                                <label for="hp">Phone Number</label>
+                                <input type="text" class="form-control" id="hp1" name="hp">
+                            </div>
+                            <div class="form-group col-md-12">
+                                <label for="service">Add Service(s)</label>
+                                <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="Room Service" id="service1" name="array">
+                                <label class="form-check-label" for="defaultCheck1">
+                                Catering / $700
+                                </label>
                                 </div>
-                            <?php } else { ?>
-                                <div>
-                                    <input type="text" value="<?php echo $_GET['type'] ?>" readonly name="tipegedung" value="tipegedung">
-                                </div> 
-                            <?php } ?>  
-                            
-                            
-                        </div>
-                        <div class="form-group col-md-12">
-                            <label for="hp">Phone Number</label>
-                            <input type="text" class="form-control" id="hp1" name="hp">
-                        </div>
-                        <div class="form-group col-md-12">
-                            <label for="service">Add Service(s)</label>
-                            <div class="form-check">
-                            <input class="form-check-input" type="checkbox" value="Room Service" id="service1" name="array[]">
-                            <label class="form-check-label" for="defaultCheck1">
-                               Catering / $700
-                            </label>
+                                <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="Decoraction" id="service2" name="array">
+                                <label class="form-check-label" for="defaultCheck1">
+                                    Decoration / $450
+                                </label>
+                                </div>
+                                <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="Sound System" id="service2" name="array">
+                                <label class="form-check-label" for="defaultCheck1">
+                                    Sound System / $250
+                                </label>
                             </div>
-                            <div class="form-check">
-                            <input class="form-check-input" type="checkbox" value="Decoraction" id="service2" name="array[]">
-                            <label class="form-check-label" for="defaultCheck1">
-                                Decoration / $450
-                            </label>
+                            
+                            <div class="form-group col-md-12">  
+                            <button type="Book" class="btn btn-primary btn-block" href="MyBooking.php">Book</button>
                             </div>
-                            <div class="form-check">
-                            <input class="form-check-input" type="checkbox" value="Sound System" id="service2" name="array[]">
-                            <label class="form-check-label" for="defaultCheck1">
-                                Sound System / $250
-                            </label>
-                        </div>
-                         
-                        <div class="form-group col-md-12">  
-                        <button type="Book" class="btn btn-primary btn-block" href="MyBooking.php">Book</button>
-                        </div>
-                    </form>
-                </div>
+                        </form>
+                    </div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="container">
-            <div class="row pt-3">
-                <div class="col text-center">
-                    <p>Created by : risma_1202194153 </p>
+            <div class="container">
+                <div class="row pt-3">
+                    <div class="col text-center">
+                        <p>Created by : risma_1202194153 </p>
+                    </div>
                 </div>
             </div>
-        </div>
-        </div>
-   </body>
-</html>
+            </div>
+    </body>
+    </html>
--- MODUL2 RISMA/MyBooking.php
+++ MODUL2 RISMA/MyBooking.php
@@ -1,107 +1,107 @@
-<?php
-    $nama = $_POST['nama'];
-    $username ="Risma_1202194153" ;
-    $eventdate = $_POST['eventdate'];
-    $strtime = $_POST ['starttime'];
-    $durasi = $_POST['duration'];
-    $cekout = date('d-m-y', strtotime('+'.$strtime. 'Hours' , strtotime($eventdate)));
-    $tipegedung = $_POST['tipegedung'];
-    $hp = $_POST['hp'];
-    $ser = 0;
-    if (empty($_POST['array'])) {
-        $service = 'No Service';
-    }else {
-        $service = $_POST['array'];
-        $ser = count($service);
-    }
-    if ($tipegedung == 'Nusantara Hall') {
-        $total = 700 * $durasi;
-    }else if ($tipegedung == 'Garuda Hall'){
-        $total = 450 * $durasi;
-    }else if ($tipegedung == 'Gedung Serba Guna'){
-        $total = 250 * $durasi;
-    }
-    $total = $total+(20*$ser);
+    <?php
+        $nama = $_POST['nama'];
+        $username ="Risma_1202194153" ;
+        $eventdate = $_POST['eventdate'];
+        $strtime = $_POST ['starttime'];
+        $durasi = $_POST['duration'];
+        $cekout = date('d-m-y', strtotime('+'.$strtime. 'Hours' , strtotime($eventdate)));
+        $tipegedung = $_POST['tipegedung'];
+        $hp = $_POST['hp'];
+        $ser = 0;
+        if (empty($_POST['array'])) {
+            $service = 'No Service';
+        }else {
+            $service = $_POST['array'];
+            $ser = count($service);
+        }
+        if ($tipegedung == 'Nusantara Hall') {
+            $total = 700 * $durasi;
+        }else if ($tipegedung == 'Garuda Hall'){
+            $total = 450 * $durasi;
+        }else if ($tipegedung == 'Gedung Serba Guna'){
+            $total = 250 * $durasi;
+        }
+        $total = $total+(20*$ser);
 
-    
-?>
+        
+    ?>
 
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
-    <title>MyBooking</title>
-  </head>
-  <body>
-       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="container d-flex justify-content-center align-item-center">
-                <div class="navbar-nav">
-                    <a class="nav-link" href="Home.php">Home <span class="sr-only">(current)</span></a>
-                    <a class="nav-link" href="Booking.php">Booking</a>
+        <title>MyBooking</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="container d-flex justify-content-center align-item-center">
+                    <div class="navbar-nav">
+                        <a class="nav-link" href="Home.php">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="Booking.php">Booking</a>
+                    </div>
+                    </div>
                 </div>
-                </div>
-            </div>
-        </nav>
-        <br>
-        <h3 class="text-center pt-3" style="padding-top: 10px;">Thank you <?= $username ?> for reserving</h3>
-        <h5 style="text-align: center; color: black">Please double check your reservation details</h5> 
-        <br>
-        <div class="container-xl mt-5 d-flex justify-content-center align-item-center">
-        <table class="table" style="width: 365mm;">
-        <thead class="thead-dark">
-            <tr>
-            <th scope="col" class="text-center">Booking Number</th>
-            <th scope="col" class="text-center">Name</th>
-            <th scope="col" class="">Check-in</th>
-            <th scope="col">Check-out</th>
-            <th scope="col">Building Type</th>
-            <th scope="col">Phone Number</th>
-            <th scope="col">Service</th>
-            <th scope="col">Total Price</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-            <th scope="row" class='text-center'><?= rand(1000000000, 9999999999) ?></th>
-            <div class="footer">
-            <td><?= $nama ?></td>
-            <td><?= $eventdate,  $strtime ?></td>
-            <td><?= $cekout, $strtime ?></td>
-            <td><?= $tipegedung ?></td>
-            <td><?= $hp ?></td>
-            <td>
-                <?php
-                if (empty($_POST['array'])) {
-                    echo $service;
-                } else {
-                    foreach ($service as $data) { 
-                    echo'<ul>';
-                        echo '<li>' . $data . '</li>';
-                    echo '</ul>' ;    
+            </nav>
+            <br>
+            <h3 class="text-center pt-3" style="padding-top: 10px;">Thank you <?= $username ?> for reserving</h3>
+            <h5 style="text-align: center; color: black">Please double check your reservation details</h5> 
+            <br>
+            <div class="container-xl mt-5 d-flex justify-content-center align-item-center">
+            <table class="table" style="width: 365mm;">
+            <thead class="thead-dark">
+                <tr>
+                <th scope="col" class="text-center">Booking Number</th>
+                <th scope="col" class="text-center">Name</th>
+                <th scope="col" class="">Check-in</th>
+                <th scope="col">Check-out</th>
+                <th scope="col">Building Type</th>
+                <th scope="col">Phone Number</th>
+                <th scope="col">Service</th>
+                <th scope="col">Total Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <th scope="row" class='text-center'><?= rand(1000000000, 9999999999) ?></th>
+                <div class="footer">
+                <td><?= $nama ?></td>
+                <td><?= $eventdate,  $strtime ?></td>
+                <td><?= $cekout, $strtime ?></td>
+                <td><?= $tipegedung ?></td>
+                <td><?= $hp ?></td>
+                <td>
+                    <?php
+                    if (empty($_GET['array'])) {
+                        echo $service;
+                    } else {
+                        foreach ($service as $data) { 
+                        echo'<ul>';
+                            echo '<li>' . $data . '</li>';
+                        echo '</ul>' ;    
+                        }
                     }
-                }
-                ?>
-            </td>
-            <td class="text-center"> $ <?= $total ?></td>
-            </tr>
-        </tbody>
-        </table>
-        </div>
-        </div>
-        <div> </div>
-        <div class="container">
-            <div class="row pt-3">
-                <div class="col text-center">
-                    <p>Created by : risma_1202194153 </p>
+                    ?>
+                </td>
+                <td class="text-center"> $ <?= $total ?></td>
+                </tr>
+            </tbody>
+            </table>
+            </div>
+            </div>
+            <div> </div>
+            <div class="container">
+                <div class="row pt-3">
+                    <div class="col text-center">
+                        <p>Created by : risma_1202194153 </p>
+                    </div>
                 </div>
             </div>
-        </div>
-  </body>
-</html>
+    </body>
+    </html>
