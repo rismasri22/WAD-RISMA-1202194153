37c689b0caa0a91ff38810a38e6ae3a3bd4714fd
rismasri22 <rismasrinh@student.telkomuniversity.ac.id>
2021-11-28 22:43:31 +0700

    Create master_data_wisata.php

---
+++ MODUL4 RISMA/setting/master_data_wisata.php
@@ -0,0 +1,27 @@
+<?php 
+	
+	define('WISATA', [
+		[
+			'gambar'	  => '1.jpg',
+			'nama_tempat' => 'Tebing Keraton',
+			'lokasi'	  => 'Bandung',
+			'deskripsi'   => 'Tiket masuk wisata dan yang lain',
+			'harga'		  => 20000
+		],
+		[
+			'gambar'	  => '2.jpg',
+			'nama_tempat' => 'Orchid Forest Cikole',
+			'lokasi'	  => 'Bandung',
+			'deskripsi'   => 'Tiket masuk wisata dan yang lain',
+			'harga'		  => 50000
+		],
+		[
+			'gambar'      => '3.jpg',
+			'nama_tempat' => 'Ranca Upas',
+			'lokasi'	  => 'Bandung',
+			'deskripsi'   => 'Paket wisata lengkap dengan Camping',
+			'harga'		  => 100000
+		]
+	])
+
+?>
