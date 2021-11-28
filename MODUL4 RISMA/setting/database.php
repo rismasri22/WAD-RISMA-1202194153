6c0667c5d5417262d68c0152065baf9a4d669c59
rismasri22 <rismasrinh@student.telkomuniversity.ac.id>
2021-11-28 22:43:36 +0700

    Create database.php

---
+++ MODUL4 RISMA/setting/database.php
@@ -0,0 +1,12 @@
+<?php
+
+$host 	  = 'localhost';
+$db 	  = 'wad_modul4';
+$username = 'root';
+$password = '';
+
+$koneksi = mysqli_connect($host, $username, $password, $db); 
+session_start();
+
+
+?>
